359910b5ba96482b2429a60a7087d7f8211ef0e7
isaac israel <email.iisrael@gmail.com>
2019-11-19 12:39:54 -0300

    Generate at the pdf on the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    result = FillPdfFormWithFakeData.call(input_file: 'lib/assets/vanhack.pdf')
+    system "open #{result.output_file}"
+    File.delete(result.output_file) if File.exist?(result.output_file)
   end
 end
