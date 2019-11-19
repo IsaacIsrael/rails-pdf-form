35b0a5aa5b298d291e9d4406c23e038990d61db5
isaac israel <email.iisrael@gmail.com>
2019-11-19 11:05:44 -0300

    Create fake data

--- app/interactors/fill_fake_data.rb
+++ app/interactors/fill_fake_data.rb
@@ -2,6 +2,13 @@ class FillFakeData
   include Interactor
 
   def call
-    # TODO
+    pdftk = PdfForms.new
+
+    context.outputs = {}
+    fields = pdftk.get_fields context.input_file
+    fields.each do |field|
+      context.outputs[field.name] = 'dummy text' if field.type == 'Text'
+      context.outputs[field.name] = field.options[0] if field.type == 'Button'
+    end
   end
 end
