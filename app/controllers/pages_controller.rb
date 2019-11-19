aa255c98d822e6a2d21f6af5204a5a17d3221f43
isaac israel <email.iisrael@gmail.com>
2019-11-19 13:42:43 -0300

    Download the pdf filled

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
-  def home
+  def home; end
+
+  def download_pdf
     result = FillPdfFormWithFakeData.call(input_file: 'lib/assets/vanhack.pdf')
-    system "open #{result.output_file}"
+    file = File.open(result.output_file)
+
+    send_data file.read,
+              filename: "filled_form.pdf",
+              type: "application/pdf"
+
     File.delete(result.output_file) if File.exist?(result.output_file)
   end
 end
