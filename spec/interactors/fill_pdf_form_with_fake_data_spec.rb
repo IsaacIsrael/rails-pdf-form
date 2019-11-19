527dd2774f59bfe79176e23afca5a4fb31de6ed4
isaac israel <email.iisrael@gmail.com>
2019-11-19 12:06:44 -0300

    Test Fill PDF form with fake data

--- spec/interactors/fill_pdf_form_with_fake_data_spec.rb
+++ spec/interactors/fill_pdf_form_with_fake_data_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 
 RSpec.describe FillPdfFormWithFakeData, type: :interactor do
   describe '.call' do
-    pending "add some examples to (or delete) #{__FILE__}"
+    let(:input_file) { 'lib/assets/vanhack.pdf'}
+    subject(:context) { FillPdfFormWithFakeData.call(input_file: input_file) }
+    it 'succeeds' do
+      expect(context).to be_a_success
+    end
+    it 'pdf fields created' do
+      expect(File.exist?(context.output_file)).to be_truthy
+      # exec("open #{context.output_file}")
+    end
   end
 end
