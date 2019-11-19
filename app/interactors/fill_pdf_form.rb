6db669e5b145a8814f17b2e681e6d756e86b6498
isaac israel <email.iisrael@gmail.com>
2019-11-19 11:40:33 -0300

    Fill the pdf form

--- app/interactors/fill_pdf_form.rb
+++ app/interactors/fill_pdf_form.rb
@@ -2,6 +2,11 @@ class FillPdfForm
   include Interactor
 
   def call
-    # TODO
+    context.output_file = 'lib/assets/output.pdf'
+    File.delete(context.output_file) if File.exist?(context.output_file)
+
+    pdftk = PdfForms.new
+    pdftk.fill_form input_file, context.output_file, context.values, flatten: true
   end
 end
+
