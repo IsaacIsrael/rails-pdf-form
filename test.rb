b286cbe18a627166776fe7c91f54b95cf0109b0c
isaac israel <email.iisrael@gmail.com>
2019-11-19 10:31:43 -0300

    Ajust the file name and the pdftk creation

--- test.rb
+++ test.rb
@@ -1,7 +1,22 @@
 require 'pdf_forms'
 
-pdftk = PdfForms.new('/usr/local/bin/pdftk')
+pdftk = PdfForms.new
+input_file = 'lib/assets/vanhack.pdf'
+output_file = 'lib/assets/output.pdf'
 
-p pdftk.get_field_names 'input_1.pdf'
-p pdftk.get_field_names 'input_2.pdf'
+# Get all the fields
+fields =  pdftk.get_fields input_file
 
+# Create fake data
+outputs = {}
+
+fields.each do |field|
+  outputs[field.name] = 'dummy text' if field.type == 'Text'
+
+  outputs[field.name] = field.options[0] if field.type == 'Button'
+end
+
+File.delete(output_file) if File.exist?(output_file)
+pdftk.fill_form input_file, output_file, outputs, flatten: true
+
+exec("open #{output_file}")
