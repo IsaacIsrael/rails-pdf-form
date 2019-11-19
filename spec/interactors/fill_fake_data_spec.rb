330ae411b0c1ed58676ccdec09d4f5ee0d4de428
isaac israel <email.iisrael@gmail.com>
2019-11-19 11:25:47 -0300

    Test the Fake data interector test

--- spec/interactors/fill_fake_data_spec.rb
+++ spec/interactors/fill_fake_data_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 RSpec.describe FillFakeData, type: :interactor do
   describe '.call' do
-    pending "add some examples to (or delete) #{__FILE__}"
+    subject(:context) { FillFakeData.call(input_file: 'lib/assets/vanhack.pdf') }
+    it 'succeeds' do
+      expect(context).to be_a_success
+    end
+    it 'return outputs for all fields' do
+      expect(context.outputs.count).to eq 454
+    end
   end
 end
