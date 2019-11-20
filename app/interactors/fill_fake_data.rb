5763af2a5f57468bab7893b93ea33c2f1e5b3397
isaac israel <email.iisrael@gmail.com>
2019-11-20 19:08:18 -0300

    Generate faker inputs

--- app/interactors/fill_fake_data.rb
+++ app/interactors/fill_fake_data.rb
@@ -7,7 +7,7 @@ class FillFakeData
     context.values = {}
     fields = pdftk.get_fields context.input_file
     fields.each do |field|
-      context.values[field.name] = 'dummy text' if field.type == 'Text'
+      context.values[field.name] = Faker::Name.unique.name if field.type == 'Text'
       context.values[field.name] = rand(0..field.options.count)  if field.type == 'Button'
     end
   end
