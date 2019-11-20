a5fa97b1565d2c4e00694edfc06bcf4d35b78729
isaac israel <email.iisrael@gmail.com>
2019-11-20 19:04:48 -0300

    Randomaze the checkbox fill

--- app/interactors/fill_fake_data.rb
+++ app/interactors/fill_fake_data.rb
@@ -8,7 +8,7 @@ class FillFakeData
     fields = pdftk.get_fields context.input_file
     fields.each do |field|
       context.values[field.name] = 'dummy text' if field.type == 'Text'
-      context.values[field.name] = field.options[0] if field.type == 'Button'
+      context.values[field.name] = rand(0..field.options.count)  if field.type == 'Button'
     end
   end
 end
